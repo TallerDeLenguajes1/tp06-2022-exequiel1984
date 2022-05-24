316f351baa485bc5d933fafe0381c00a1b2ca19a
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-05-24 14:59:51 -0300

    punto 1

---
+++ Calculadora.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace punto1{
+    
+    public class Calculadora {
+
+        private Double resultado;
+
+        public double Resultado { get => resultado; set => resultado = value; }
+
+        public Calculadora(double numero)
+        {
+            Resultado = numero;
+        }
+
+        public void Sumar(Double termino){
+            Resultado += termino;
+        }
+
+        public void Restar(Double termino){
+            Resultado -= termino;
+        }
+        
+        public void Multiplicar(Double termino){
+            Resultado *= termino;
+        }
+
+        public void Dividir(Double termino){
+            
+            Resultado /= termino;
+        }
+    }
+}
+
