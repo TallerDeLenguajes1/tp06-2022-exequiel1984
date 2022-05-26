4e461b2a87a5c83b672d7a595b06ee4cdf89ebe3
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-05-26 18:15:41 -0300

    corrijo libreria namespace

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿using Empleado;
+﻿using ejercicio2;
 
-Empleado exequiel = new Empleado("Exequiel", "Carrizo", new DateTime(1984, 10, 13), 'H', 'C', new DateTime(2020, 20, 3), 65000, Empleado.Cargo.Auxiliar);
+Empleado exequiel = new Empleado("Exequiel", "Carrizo", new DateTime(1984, 10, 13), 'H', 'C', new DateTime(2020, 20, 3), 65000, Empleado.cargos.Auxiliar);
 
 mostrarInfoEmpleado(exequiel);
